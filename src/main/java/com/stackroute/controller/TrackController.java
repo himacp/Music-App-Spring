0d433777fcd0940d50e5d4de378b9cec317ba162
hima <himacp2396@gmail.com>
2019-01-22 14:47:09 +0530

    h2-console added

--- src/main/java/com/stackroute/controller/TrackController.java
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -21,23 +21,19 @@ public class TrackController {
     public List<Track> retrieveAllTracks() {
 
         return trackRepository.findAll();
-
     }
 
     @GetMapping("/track/{id}")
-
     public Track retrieveTrack(@PathVariable int id)
     {
         Optional<Track> track = trackRepository.findById(id);
         return track.get();
     }
-
     @DeleteMapping("/track/{id}")
-
-    public void deleteTrack(@PathVariable int id) {
+    public void deleteTrack(@PathVariable int id)
+    {
 
         trackRepository.deleteById(id);
-
     }
 
     @PostMapping("/track")
@@ -45,33 +41,26 @@ public class TrackController {
     public ResponseEntity<Object> createTrack(@RequestBody Track track) {
 
         ResponseEntity responseEntity;
-
         try
-
         {
             trackRepository.save(track);
 
             responseEntity=new ResponseEntity<String>("Successfully created", HttpStatus.CREATED);
 
         }catch(Exception ex)
-
         {
             responseEntity=new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
         }
 
         return responseEntity;
-
     }
 
     @PutMapping("/track/{id}")
-
     public ResponseEntity<Object> updateTrack(@RequestBody Track track, @PathVariable int id) {
 
         Optional<Track> trackOptional = trackRepository.findById(id);
-
         if (!trackOptional.isPresent())
             return ResponseEntity.notFound().build();
-
         track.setTrackId(id);
         trackRepository.save(track);
         return ResponseEntity.noContent().build();
