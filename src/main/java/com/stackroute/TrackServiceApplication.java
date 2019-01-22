7ac99bd2c4ed16eb92db6e8a62d93515219b15a0
hima <himacp2396@gmail.com>
2019-01-22 23:54:59 +0530

    content refresh

--- src/main/java/com/stackroute/TrackServiceApplication.java
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -1,12 +1,36 @@
+
 package com.stackroute;
 
+import com.stackroute.domain.Track;
+import com.stackroute.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 @SpringBootApplication
-public class TrackServiceApplication {
-    public static void main(String[] args) {
-            SpringApplication.run(TrackServiceApplication.class, args);
+public class TrackServiceApplication implements ApplicationListener<ContextRefreshedEvent> , CommandLineRunner {
+
+        @Autowired
+        TrackRepository trackRepository;
+        public static void main(String[] args) {
+                SpringApplication.run(TrackServiceApplication.class, args);
         }
-    }
+
+
+        @Override
+        public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+                trackRepository.save(new Track("himaa",34,"error"));
+        }
+
+        @Override
+        public void run(String...args) throws Exception {
+                trackRepository.save(new Track("prabha",35,"highfive"));
+        }
+}
 
 
