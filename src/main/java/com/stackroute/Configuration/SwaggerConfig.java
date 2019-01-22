90a6870b9960ebdf166e8d916bc404a2a118acf1
hima <himacp2396@gmail.com>
2019-01-22 17:31:27 +0530

    commit

--- src/main/java/com/stackroute/Configuration/SwaggerConfig.java
+++ src/main/java/com/stackroute/Configuration/SwaggerConfig.java
@@ -7,12 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.nio.channels.Selector;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -24,8 +27,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-                .paths(regex("/product.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.stackroute"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
     }
