92d9f9a31d9705261ae677c8b894acce9caa3eb0
hima <himacp2396@gmail.com>
2019-01-23 10:34:35 +0530

    Enivronment commit

--- src/main/java/com/stackroute/TrackServiceApplication.java
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -4,32 +4,54 @@ package com.stackroute;
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+
+
+@PropertySource("classpath:application.properties")
 @Component
 @SpringBootApplication
 public class TrackServiceApplication implements ApplicationListener<ContextRefreshedEvent> , CommandLineRunner {
 
+
+    @Value("${trackId}")
+        private int trackId;
+
+
+        @Value("${trackName}")
+        private String trackName;
+
+
+        @Value("${trackComments}")
+        private String trackComments;
+
         @Autowired
         TrackRepository trackRepository;
         public static void main(String[] args) {
-                SpringApplication.run(TrackServiceApplication.class, args);
+            SpringApplication.run(TrackServiceApplication.class, args);
         }
 
 
         @Override
         public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-                trackRepository.save(new Track(1,"hima","error"));
+            trackRepository.save(new Track(Integer.parseInt(ev.getProperty("trackId")),ev.getProperty("trackName"),ev.getProperty("trackComments")));
+
         }
+    @Autowired
+    private Environment ev;
 
         @Override
         public void run(String...args) throws Exception {
-                trackRepository.save(new Track(12,"prabha","highfive"));
+            trackRepository.save(new Track(trackId,trackName,trackComments));
+
         }
 }
 
